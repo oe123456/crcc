01a658a62ed03662b0e13fee018c092bd65bd899
oe123456 <oe123456@163.com>
2021-09-24 10:40:01 +0800

    test

---
+++ socketUtils/src/main/java/com/crcc/socketutils/CrccHelper.java
@@ -0,0 +1,11 @@
+package com.crcc.socketutils;
+
+import android.util.Log;
+
+public class CrccHelper {
+
+    public static void showTest(String str) {
+        Log.e("###", "###" + str + " showTest");
+
+    }
+}
